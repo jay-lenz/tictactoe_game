34f9aa817b64c4a8364654cf23f30439789e7af0
Jenyo Oyebode <jenyooyebode@icloud.com>
2023-07-29 10:31:25 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacChallenge
+{
+    class Program : TicTacSolution
+    {
+        static void Main(string[] args)
+        {
+            var c = new TicTacSolution();
+            TicTac();
+        }
+    }
+}
